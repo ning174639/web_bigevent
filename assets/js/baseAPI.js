c5230876ab7b7612a8c5a949d5e66d0b50d0bd2a
ning <1746394429@qq.com>
2020-08-01 14:36:23 +0800

    完成了主页功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -2,7 +2,24 @@
 var baseURL = 'http://ajax.frontend.itheima.net'
 //设置路径（生产）
 
-//拦截/过滤每一次ajax请求，配置每次请求需要的参数
+//1.拦截/过滤每一次ajax请求，配置每次请求需要的参数
 $.ajaxPrefilter(function (options) {
     options.url = baseURL + options.url
+
+    //2.判断请求路径是否包含 /my/
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || '',
+        }
+    }
+
+    //3.所有的请求完成后都要对身份进行判断
+    options.complete = function (res) {
+        console.log(res)
+        var data = res.responseJSON
+        if (data.status == 1 && data.message == '身份认证失败！') {
+            localStorage.removeItem('token')
+            location.href = '/login.html'
+        }
+    }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,54 @@
+$(function () {
+    //调用函数获取用户信息
+    getUserInfo()
+
+    // 3.点击退出登录
+    //引用layer
+    var layer = layui.layer
+    $('#btnLogout').on('click', function () {
+        layer.confirm('是否确认退出?', { icon: 3, title: '提示' }, function (
+            index
+        ) {
+            layer.close(index)
+            localStorage.removeItem('token')
+            location.href = '/login.html'
+        })
+    })
+})
+
+//获取用户信息
+function getUserInfo() {
+    $.ajax({
+        url: '/my/userinfo',
+        //Jquery中的ajax,专门用来设置请求头的信息的属性  headers区分大小写
+        // headers: {
+        //     Authorization: localStorage.getItem('token') || '',
+        // },
+        success: function (res) {
+            console.log(res)
+            if (res.status !== 0) {
+                return layui.layer.msg(res.message)
+            }
+            //调用用户渲染的头像
+            renderUser(res.data)
+        },
+        //不论成功还是失败，都会调用complete回调函数
+        // compltete: function (res) {},
+    })
+}
+
+//封装用户头像数据
+function renderUser(user) {
+    //1.渲染用户名
+    var uname = user.nickname || user.username
+    $('#weclome').html('欢迎&nbsp;&nbsp;' + uname)
+
+    // 2.渲染头像  如果为空 渲染文本头像
+    if (user.user_pic != null) {
+        $('.layui-nav-img').attr('src', user.user_pic).show()
+        $('.text-avatar').hide()
+    } else {
+        $('.layui-nav-img').hide()
+        $('.text-avatar').html(uname[0].toUpperCase()).show()
+    }
+}
